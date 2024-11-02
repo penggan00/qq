767e91fe0e48d99b195c6faf16980290966a49b7
好酒 <106035465+penggan00@users.noreply.github.com>
2024-11-02 15:16:56 +0800

    更新 setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,10 @@ mv qq.sh ~/
 chmod +x ~/qq.sh
 chmod +x ~/qq/setup.sh
 
+# 为 crontab 添加定时任务（每 5 分钟运行一次）
+echo "添加 crontab 任务..."
+(crontab -l 2>/dev/null; echo "*/5 * * * * ~/qq.sh") | crontab -
+
 # 初始化 npm 项目
 echo "初始化 npm 项目..."
 npm init -y
@@ -21,12 +25,6 @@ if [[ -f "package.json" ]]; then
 else
     echo "错误：未找到 package.json 文件。"
 fi
-
-# 为 crontab 添加定时任务（每 5 分钟运行一次）
-echo "添加 crontab 任务..."
-(crontab -l 2>/dev/null; echo "*/5 * * * * ~/qq.sh") | crontab -
-
 echo "项目安装完成！"
-
 # 打开 .env 文件以便输入
 nano ~/qq/.env
